f1fdb985234c0ba7e66caaa1649845bfc4fdd707
kwYoohae <gocksdl7979@gmail.com>
2022-12-26 15:23:54 +0900

    feat: 프로젝트 구성

---
+++ src/test/java/com/sanhak/l2cache/L2cacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanhak.l2cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L2cacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
